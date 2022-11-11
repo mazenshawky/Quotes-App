d621aecda72c36be7719eecf53919c93038b76b6
Mazen Shawky <mazenshawqy@gmail.com>
2022-11-11 02:45:44 +0200

    repository

---
+++ lib/features/random_quote/domain/repositories/quote_repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:quotes_app/features/random_quote/domain/entities/quote.dart';
+
+import '../../../../core/utils/error/failures.dart';
+
+abstract class QuoteRepository {
+  Future<Either<Failure, Quote>> getRandomQuote();
+}
--- lib/features/random_quote/domain/usecases/get_random_quote.dart
+++ lib/features/random_quote/domain/usecases/get_random_quote.dart
@@ -1,13 +1,16 @@
 import 'package:dartz/dartz.dart';
 import 'package:quotes_app/core/usecases/usecase.dart';
+import 'package:quotes_app/features/random_quote/domain/repositories/quote_repository.dart';
 
 import '../../../../core/utils/error/failures.dart';
 import '../entities/quote.dart';
 
 class GetRandomQuote implements UseCase<Quote, NoParams> {
+  final QuoteRepository quoteRepository;
+
+  GetRandomQuote({required this.quoteRepository});
+
   @override
-  Future<Either<Failure, Quote>> call(NoParams params) {
-    // TODO: implement call
-    throw UnimplementedError();
-  }
+  Future<Either<Failure, Quote>> call(NoParams params) =>
+      quoteRepository.getRandomQuote();
 }
